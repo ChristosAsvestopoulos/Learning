c67d15ed2bcad688afb8ec5c6e5f05b1d5f4f9d0
Christos Asvestopoulos <c.asvestopoulos@kaizengaming.com>
2024-04-13 16:14:33 +0300

    add docker and rabbitMQ demos

---
+++ rabbitmq-test/RabbitMQDemo/RabbitReceiver2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+ConnectionFactory factory = new()
+{
+    Uri = new Uri("amqp://guest:guest@localhost:5672"),
+    ClientProvidedName = "Rabbit Receiver2 App"
+};
+
+IConnection cnn = factory.CreateConnection();
+
+IModel channel = cnn.CreateModel();
+
+string exchangeName = "DemoExchange";
+string routingKey = "demo-routing-key";
+string queueName = "DemoQueue";
+
+channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+channel.QueueDeclare(queueName, false, false, false, null);
+channel.QueueBind(queueName, exchangeName, routingKey, null);
+
+channel.BasicQos(0, 1, false);
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (sender, args) =>
+{
+    Task.Delay(TimeSpan.FromSeconds(3)).Wait();
+    var body = args.Body.ToArray();
+    string message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Message Received: {message}");
+    channel.BasicAck(args.DeliveryTag, false);
+};
+
+string consumerTag = channel.BasicConsume(queueName, false, consumer);
+Console.ReadLine();
+
+channel.Close();
+cnn.Close();
